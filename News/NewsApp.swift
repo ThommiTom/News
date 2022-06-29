5ac8299537b63caee92eecf55d1bbd5c704a0bda
schatton <thomas.schatton@fkfs.de>
2022-06-29 10:46:14 +0200

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Thomas Schatton on 29.06.22.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
