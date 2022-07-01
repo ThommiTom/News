c5d4557c2ef414758152e471a17414ea710c8d61
schatton <t.schatton@me.com>
2022-07-01 11:16:15 +0200

    Testing GIT Contribution Graph

--- News/Networking/NetworkManager.swift
+++ News/Networking/NetworkManager.swift
@@ -5,10 +5,14 @@
 //  Created by Thomas Schatton on 30.06.22.
 //
 
+// TESTING GIT SETTING FOR CONTRIBUTION GRAPH
+
 import Foundation
 
 class NetworkManager {
     static let shared = NetworkManager()
     
     private init() {}
+    
+    // TODO: implement NetworkManager!
 }
